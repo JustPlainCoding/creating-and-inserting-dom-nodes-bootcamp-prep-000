7542b04d47af7ba3cd90b1f8abf821be18f822f8
Eric J Wilhite <ericjwilhite@gmail.com>
2019-05-23 22:31:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -5,4 +5,4 @@ var element = document.createElement('div');
 element.innerHTML = 'Hello, DOM!';
 element.style.backgroundColor = "#f9f9f9";
 document.body.appendChild(element);
-element.style.textAlign('center');
+element.style.textAlign = 'center';
